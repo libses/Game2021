8905bab1ac27d0f6f9daced38f9eeb2ad8b1e809
Victor <78375906+vkapkaev@users.noreply.github.com>
2021-03-22 10:18:30 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿//302578XJ
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Program {
+    class Program
+    { 
+        static void Print(int[,] array, int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(array[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static void Main()
+        {
+            var input = Console.ReadLine().Split();
+            var n = int.Parse(input[0]);
+            var startVertex = int.Parse(input[1]);
+
+            var matrix = new int[n,n];
+
+            for(int i = 0; i < n; i++)
+            {
+                var strArr = Console.ReadLine().Split();
+                for(int j = 0; j < n; j++)
+                {
+                    matrix[i, j] = int.Parse(strArr[j]);
+                }
+            }
+
+            Print(matrix, n);
+        }
+    }
+}
