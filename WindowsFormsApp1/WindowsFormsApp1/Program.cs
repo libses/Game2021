5ea93afb4e36774d73a228f9c2a0593bb1f41311
DA.Morozov@urfu.me <DA.Morozov@urfu.me>
2021-03-23 19:39:10 +0500

    made comment

--- WindowsFormsApp1/WindowsFormsApp1/Program.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Program.cs
@@ -14,6 +14,7 @@ namespace WindowsFormsApp1
         [STAThread]
         static void Main()
         {
+            //assssss
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
