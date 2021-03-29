b7f2a1b592f64e9dfb18753536b6872ad399066f
sadovnichek <kapkaevictor@gmail.com>
2021-03-29 15:50:44 +0500

    add test

--- WindowsFormsApp1/WindowsFormsApp1/Geometry.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Geometry.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using NUnit.Framework;
 
 namespace WindowsFormsApp1
 {
     class Vector : Point
     {
-        public Vector(float x, float y)
+        public Vector(float x, float y) : base(x, y)
         {
             X = x;
             Y = y;
@@ -17,13 +18,32 @@ namespace WindowsFormsApp1
 
     class Segment
     {
-        Point Start;
-        Point End;
+        public Point Start;
+        public Point End;
     }
 
     class Point
     {
         public float X;
         public float Y;
+
+        public Point(float x, float y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+
+    [TestFixture]
+    class Tests
+    {
+        [Test]
+        public void InitializationTest()
+        {
+            var p = new Point(1,1);
+            var v = new Vector(1, 1);
+            Assert.IsTrue(p is Point);
+            Assert.IsTrue(v is Vector);
+        }
     }
 }
