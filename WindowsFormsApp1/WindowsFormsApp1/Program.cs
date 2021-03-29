e86ba0b056339f86b93ad0bf7c8cb12b63c06258
DA.Morozov@urfu.me <DA.Morozov@urfu.me>
2021-03-29 10:07:09 +0500

    THE start

--- WindowsFormsApp1/WindowsFormsApp1/Program.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Program.cs
@@ -6,24 +6,11 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApp1
 {
-    public class Dima
-    {
-        string name;//12345
-    }
-    public class Player
-    {
-        private string name;
-    }
-
     static class Program
     {
-        /// <summary>
-        /// Главная точка входа для приложения.
-        /// </summary>
         [STAThread]
         static void Main()
         {
-            //assssss!ssss
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
