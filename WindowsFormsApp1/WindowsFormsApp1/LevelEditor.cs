b1b9717cff577a33113cdc1a4cef4c363d89ab50
sadovnichek <kapkaevictor@gmail.com>
2021-04-02 22:36:45 +0500

    complete a Level editor

--- WindowsFormsApp1/WindowsFormsApp1/LevelEditor.cs
+++ WindowsFormsApp1/WindowsFormsApp1/LevelEditor.cs
@@ -1,18 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace WindowsFormsApp1
 {
-    public class Level
+	public enum Block
     {
-        public readonly int[,] Map; // int - на время, нужно придумать тип для элеиента блока(grouund, etc.)
-        
-        public Level(string[] lines)
-        {
-            //
-        }
+		Ground,
+		Empty
     }
+
+	public class Level
+	{
+		public readonly Block[,] Map;
+		public readonly Point Start;
+
+		private Level(Block[,] map, Point start)
+		{
+			Map = map;
+			Start = start;
+		}
+
+		public static Level FromText(string text)
+		{
+			var lines = text.Split(new[] { "\r", "\n" }, StringSplitOptions.RemoveEmptyEntries);
+			return FromLines(lines);
+		}
+
+		public static Level FromLines(string[] lines)
+		{
+			var map = new Block[lines[0].Length, lines.Length];
+			var start = Point.Empty;
+			for (var y = 0; y < lines.Length; y++)
+			{
+				for (var x = 0; x < lines[0].Length; x++)
+				{
+					switch (lines[y][x])
+					{
+						case 'G':
+							map[x, y] = Block.Ground;
+							break;
+						default:
+							map[x, y] = Block.Empty;
+							break;
+					}
+				}
+			}
+			return new Level(map, start);
+		}
+	}
 }
