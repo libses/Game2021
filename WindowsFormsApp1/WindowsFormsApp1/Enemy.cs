e401e5d0ecf424fd32c7ff735d06caf4a29cefa4
Victor <78375906+sadovnichek@users.noreply.github.com>
2021-05-17 10:30:05 +0500

    Добавлен алгоритм Брезенхама

--- WindowsFormsApp1/WindowsFormsApp1/Enemy.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Enemy.cs
@@ -26,7 +26,9 @@ namespace WindowsFormsApp1
             IsLeft = false;
             IsRight = false;
             var path = player.Location - Location;
-            if (path.Length >= 20 && path.Length < 200)
+            var pathDerscrete = BresenhamAlgorithm((Location - new Vector(10, 10)) * 0.05, (player.Location - new Vector(10, 10)) * 0.05, Map);
+            bool hasGround = pathDerscrete.Contains(Block.Ground);
+            if (path.Length >= 20 && path.Length < 200 && hasGround)
             {
                 //if (random.NextDouble() < 0.7)
                 //{
@@ -60,7 +62,7 @@ namespace WindowsFormsApp1
                 if (path.X < 0 && path.Y >= 0)
                     IsLeft = true;
             }
-            else if (path.Length >= 200)
+            else if (path.Length >= 200 || hasGround)
                 OrdinaryMove();
         }
 
@@ -80,6 +82,36 @@ namespace WindowsFormsApp1
             }
             if (action == 3)
                 IsJump = true;
+        }
+        
+        public IEnumerable<Block> BresenhamAlgorithm(Vector start, Vector end, Block[,] map)
+        {
+            var x0 = start.X;
+            var x1 = end.X;
+            var y0 = start.Y;
+            var y1 = end.Y;
+            var dx = Math.Abs(x1 - x0);
+            var sx = x0 < x1 ? 1 : -1;
+            var dy = -Math.Abs(y1 - y0);
+            var sy = y0 < y1 ? 1 : -1;
+            var err = dx + dy;  /* error value e_xy */
+            while (true)
+            {
+                yield return map[x0, y0];
+                if (x0 == x1 && y0 == y1)
+                    break;
+                var e2 = 2 * err;
+                if (e2 >= dy)/* e_xy+e_x > 0 */
+                {
+                    err += dy;
+                    x0 += sx;
+                }
+                if (e2 <= dx)/* e_xy+e_y < 0 */
+                {
+                    err += dx;
+                    y0 += sy;
+                }
+            }
         }
     }
 }
